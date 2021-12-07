8a25f519bb65abd158ee707dcfa466ba729ddd9d
Nolan Y? <ndymario@gmail.com>
2021-12-07 13:20:39 -0600

    Begin Implementation of Terminal GUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <string>
 #include "levelParser.h"
-
-using namespace std;
-
-string gameDir = "../smb3-master";
-
-int main()
-{
-    return 0;
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <iostream>
+ 
+int main(void) {
+  using namespace ftxui;
+ 
+  // Define the document
+  Element document =
+    hbox({
+      text("Left")   | border,
+      text("Middle") | border | flex,
+      text("Right")  | border,
+    });
+ 
+  auto screen = Screen::Create(
+    Dimension::Full(),       // Width
+    Dimension::Fit(document) // Height
+  );
+  Render(screen, document);
+  screen.Print();
+ 
+  return EXIT_SUCCESS;
 }
